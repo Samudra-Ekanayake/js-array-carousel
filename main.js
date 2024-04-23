eb65ed8b2253fd5dcb56dd66ec86ea54ce14fecd
Samudra-Ekanayake <samudra2724@gmail.com>
2024-04-23 21:50:10 +0200

    M3 - completato

--- main.js
+++ main.js
@@ -3,3 +3,23 @@ let slides = document.getElementsByClassName("slide")
 let immagineAttiva
 let immagineSuccessiva
 
+document.getElementById("successiva").addEventListener("click", function () {
+
+    for (let i = 0; i < slides.length; i++) {
+
+        const slide = slides[i];
+
+        if (slide.classList.contains("active")) {
+            slide.classList.remove("active")
+            immagineAttiva = i
+        }
+    }
+
+    immagineSuccessiva = immagineAttiva + 1
+
+    if (immagineSuccessiva >= slides.length) {
+        immagineSuccessiva = 0
+    }
+
+    slides[immagineSuccessiva].classList.add("active")
+});
--- readme.md
+++ readme.md
@@ -2,3 +2,6 @@ fatto il lyout
 sistemato il css
 inziato codicie js
 dichiarato le variabili
+inizio a scrivere il ciclo
+messo pulsanti su html
+riprendo su js
